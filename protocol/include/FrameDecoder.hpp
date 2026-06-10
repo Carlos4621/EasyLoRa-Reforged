18ea09559cf3b72d9348598ecc7656526a300604
Carlos Iván <carlosivanhb@hotmail.com>
2026-06-10 11:27:10 -0600

    Documentada la necesitar de que el buffer del payload debe vivir mientras se usa el frame

--- protocol/include/FrameDecoder.hpp
+++ protocol/include/FrameDecoder.hpp
@@ -27,6 +27,7 @@ public:
     /// @return std::expected con el frame en caso de éxito, ProtocolErrors en caso de error
     /// @warning Se debe entregar el buffer SIN el 0x00
     /// @warning El buffer del payload no puede ser usado como salida
+    /// @warning Debido a que Frame contiene un span, el buffer de payloadInFrame debe vivir cuando se usa el frame
     [[nodiscard]]
     static std::expected<Frame, ProtocolErrors> decode(std::span<const uint8_t> inputRawBuffer, std::span<uint8_t> payloadInFrame) noexcept;
 
